0981fadcc78a379716b294e45b3b9d11a6d45d87
silencepeaceoff <silencepeaceoff@gmail.com>
2024-04-11 15:28:16 +0400

    Delete personal api key

--- Services/CoinDataService.swift
+++ Services/CoinDataService.swift
@@ -20,7 +20,7 @@ final class CoinDataService {
   }
 
   func getCoins() {
-    guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en&x_cg_demo_api_key=CG-CGhw4JokFzv2P6kbQtCbKkuK"
+    guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en"
     ) else { return }
 
     let decoder = JSONDecoder()
